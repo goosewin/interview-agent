d10c9c501db3c98725f768f929724194416564a4
goosewin <daniel@webline.app>
2025-02-22 12:01:54 -0800

    feat: add vercel blob

---
+++ lib/blob.ts
@@ -0,0 +1,16 @@
+import { put } from "@vercel/blob";
+
+export const uploadFileToBlob = async (file: File) => {
+  const { url } = await put(file.name, file, { access: 'public' });
+  return url;
+};
+
+export const downloadFileFromBlob = async (url: string) => {
+  const response = await fetch(url);
+  return response.blob();
+};
+
+export const deleteFileFromBlob = async (url: string) => {
+  const response = await fetch(url, { method: 'DELETE' });
+  return response.json();
+};
